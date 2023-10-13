f0a2fbadb88a5ffc6593dfe612f942cd3adff533
m4vinayak <m4vinayak@gmail.com>
2023-10-13 14:51:36 +0530

    addition file

---
+++ Test/src/com/demopackage1/Addition.java
@@ -0,0 +1,12 @@
+package com.demopackage1;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println("addition is : "+sum);
+	}
+
+}
